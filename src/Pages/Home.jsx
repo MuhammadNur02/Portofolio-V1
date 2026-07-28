be97f08e882cf84fb9b02c5be35c6154d4fd6352
MuhammadNur02 <muchammad.nur02@gmail.com>
2026-07-28 20:36:15 +0700

    update deskripsi

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -205,7 +205,7 @@ const Home = () => {
                   <p className="text-base md:text-lg text-gray-400 max-w-xl leading-relaxed font-light"
                     data-aos="fade-up"
                     data-aos-delay="1000">
-                    Menciptakan Website Yang Inovatif, Fungsional, dan User-Friendly untuk Solusi Digital.
+                    Combining modern front-end architecture, visual aesthetics, and UX principles to build a game interface that is responsive, smooth, and intuitive. Focusing on creating HUDs, menu systems, and immersive player interactions.
                   </p>
 
                   {/* Tech Stack */}
